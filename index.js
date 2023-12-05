b0380631ce21ab45acbda46861b0d97d36e1c2ca
gucchunchun <yunataniguchi23@gmail.com>
2023-12-05 18:00:50 +0900

    [Update] Prevent Hamburger Animation After Page Loading
    Hamburger animation was happening just after page loading\n=>prevent the animation happening by managing the animatinon with toggling 'animated' class

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+// FUNCTIONS
 function toggleNavMenu() {
+    if(!navToggleBtn.classList.contains('animated')){
+        navToggleBtn.classList.add('animated');
+    }
     navToggleBtn.classList.toggle('active');
     navSmallScreen.classList.toggle('active');
 }
 
+// GET DOM ELEMENT
+const body = document.querySelector('body');
 const navToggleBtn = document.querySelector('#navToggleBtn');
 const navSmallScreen = document.querySelector('#navSmallScreen');
 
+// to toggle navigation menu
 navToggleBtn.addEventListener('click', toggleNavMenu);
