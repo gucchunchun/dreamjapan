4d4c37561f08b408586c742ffed86a13c7f41de1
gucchunchun <yunataniguchi23@gmail.com>
2023-12-13 00:10:15 +0900

    [Add] Other Page Decoration & Change Nav Open Animaton
    Change Nav Open Animaton:\n=>add fucntion to add 'nav' to body class when the navigation is open. position ticket link in the navigation menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // To toggle navigation menu
+const body = document.querySelector('body');
 const navToggleBtn = document.getElementById('navToggleBtn');
 const navSmallCtr = document.getElementById('navSmallCtr');
 const navSmallLists = document.querySelectorAll('.nav-list--small');
@@ -7,6 +8,7 @@ function openNavMenu() {
     navToggleBtn.classList.add('animated');
     navToggleBtn.classList.add('active');
     navSmallCtr.classList.add('active');
+    body.classList.add('nav');
     navToggleBtn.setAttribute('aria-label', 'close navigation menu');
     navToggleBtn.removeEventListener('click', openNavMenu);
     navToggleBtn.addEventListener('click', closeNavMenu);
@@ -15,6 +17,7 @@ function openNavMenu() {
 function closeNavMenu() {
     navToggleBtn.classList.remove('active');
     navSmallCtr.classList.remove('active');
+    body.classList.remove('nav');
     navToggleBtn.setAttribute('aria-label', 'open navigation menu');
     navToggleBtn.removeEventListener('click', closeNavMenu);
     navToggleBtn.addEventListener('click', openNavMenu);
